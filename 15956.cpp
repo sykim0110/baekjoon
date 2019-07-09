e9bb783df59bea68f0a6c3ea2439ebd93302602d
yun0110 <malco2357@gmail.com>
2019-07-09 13:35:27 +0900

    modified 15956

--- 15956.cpp
+++ 15956.cpp
@@ -10,6 +10,8 @@
 #define NOTEQ 1
 using namespace std;
 
+
+//index의 first에서 해당 단어가 있는 지 확인. 있으면 해당 position 반환, 없으면 -1 반환
 int search(vector< pair< vector<string>, vector<string> > >& index, string word) {
     for(int i = 0; i < index.size() ; ++i) {
         if(find(index[i].first.begin(), index[i].first.end(), word) != index[i].first.end()) {
@@ -19,7 +21,49 @@ int search(vector< pair< vector<string>, vector<string> > >& index, string word)
     return -1;
 }
 
+int countNumber(vector<string> input) {
+    bool flag = true;
+    int count = 0;
+    for(const string& s : input) {
+        flag = true;
+        if(s.size() > 1){
+            for(int i = 1;i < s.size() ; i++) {
+                if((int)s[i] > 9) {
+                    flag = false;
+                } 
+            }
+        }
+        else {
+            if((int)s[0] > 9)
+                flag = false;
+        }
+        if(flag) {
+            count++;
+        }
+        
+    }
+
+    return count;
+}
+
+// string vector에 들어있는 string 중, 길이가 가장 짧은 string 반환
+pair<string, int> findShortest(vector<string> list) {
+    size_t threshold = -1;
+    string shortest;
+    int index = -1;
+    for(int i = 0 ; i < list.size() ; ++i) {
+        if(list[i].length() < threshold) {
+            threshold = list[i].length();
+            shortest = list[i];
+            index = i;
+        }
+    }
+
+    return make_pair(shortest, index);
+}
+
 void solve(vector<pair< pair<string, string>, int> >& set) {
+    vector<string> ans;
     vector< pair< vector<string>, vector<string> > > index;
     string word1, word2;
     int flag;
@@ -27,45 +71,136 @@ void solve(vector<pair< pair<string, string>, int> >& set) {
         word1 = (*it).first.first;
         word2 = (*it).first.second;
         flag = (*it).second;
-        
-        if(index.size() == 0) {
-            pair< vector<string>, vector<string> > temp;
-            temp.first.push_back(word1);
-            if(flag == EQ)
-                temp.first.push_back(word2);
-            else
-                temp.second.push_back(word2);
-            
-            index.push_back(temp);
-        }
-        else {
-            int where = search(index, word1);
-
-            if(where != -1) {
-                if(flag == EQ)
-                    index[where].first.push_back(word2);
-                else
-                    index[where].second.push_back(word2);
+        if(word1 == word2) {
+            if(flag == EQ) {
+                int where = search(index, word1);
+                if(where == -1) {
+                    pair< vector<string>, vector<string> > temp;
+                    temp.first.push_back(word1);
+                    index.push_back(temp);
+                }                
             }
             else {
-                int where2 = search(index, word2);
-                if(where2 != -1) {
-                    if(flag == EQ)
-                        index[where2].first.push_back(word1);
-                    else    
-                        index[where2].second.push_back(word2);
-                }
+                cout << "1==0" << endl;
+                return;
+            }
+        }
+        else {
+            if(index.size() == 0) {
                 pair< vector<string>, vector<string> > temp;
                 temp.first.push_back(word1);
                 if(flag == EQ)
                     temp.first.push_back(word2);
                 else
                     temp.second.push_back(word2);
+                
                 index.push_back(temp);
+            }
+            else {
+                int where = search(index, word1);
+                int where2 = search(index, word2);
+                if(flag == EQ) {
+                    // 둘 다 처음 나오는 단어인 경우
+                    if(where == -1 && where2 == -1) {
+                        pair< vector<string>, vector<string> > temp;
+                        temp.first.push_back(word1);
+                        temp.first.push_back(word2);
+                        index.push_back(temp);
+                    }
+                    // word2만 처음 나오는 단어인 경우
+                    else if(where != -1 && where2 == -1) {
+                        index[where].first.push_back(word2);
+                    }
+                    // word1만 처음 나오는 단어인 경우
+                    else if(where == -1 && where2 != -1) {
+                        index[where2].first.push_back(word1);
+                    }
+                    // 둘 다 이미 나온 경우
+                    else {
+                        // possible?
+                    }
+                }
+                else { // flag == NOTEQ
+                    // 둘 다 처음 나오는 경우
+                    if(where == -1 && where2 == -1) {
+                        pair< vector<string>, vector<string> > temp;
+                        temp.first.push_back(word1);
+                        temp.second.push_back(word2);
+                        index.push_back(temp);
+                    }
+                    // word2만 처음 나오는 경우
+                    else if(where != -1 && where2 == -1) {
+                        // 먼저, index[where].second에 word2가 있는지 확인
+                        // 있다면 - Pass
+                        // 없다면 - 추가
+                        if(find(index[where].second.begin(), index[where].second.end(), word2) == index[where].second.end()){
+                            index[where].second.push_back(word2);
+                        }                  
+                    }
+                    else if(where == -1 && where2 != -1) {
+                        // 먼저, index[where2].second에 word1가 있는지 확인
+                        // 있다면 - Pass
+                        // 없다면 - 추가
+                        if(find(index[where2].second.begin(), index[where2].second.end(), word1) == index[where2].second.end()){
+                            index[where2].second.push_back(word1);
+                        }
+                    }
+                    // 둘 다 이미 나온 경우
+                    // Case 1. 같은 곳에 존재 - 모순!
+                    // Case 2. 다른 곳에 존재 - 둘 중에 한 군데만 추가하면 됨.
+                    else {
+                        if(where == where2) {
+                            cout << "1==0"<< endl;
+                            return;
+                        }
+                        if(find(index[where].second.begin(), index[where].second.end(), word2) != index[where].second.end()){
+                            continue;
+                        }
+                        if(find(index[where2].second.begin(), index[where2].second.end(), word1) == index[where2].second.end()){
+                            continue;
+                        }
+                        index[where].second.push_back(word2);
+                    }
+                }   
             }
         }
     }
 
+    // 출력
+    if(index.size() == 1) {
+        if(index[0].first.size() == 1 && index[0].second.size() == 0){
+            cout << "0==0" << endl;
+            return;
+        }
+    }
+    for(const pair< vector<string>, vector<string> >& p : index) {
+        cout << "count is "<< countNumber(p.first) << endl;        
+        if(countNumber(p.first) > 1) {
+            cout << "1==0" << endl;
+            return;
+        }
+        pair<string, int> shortestInfo = findShortest(p.first);
+        for(int i = 0 ; i < p.first.size(); ++i) {
+            if(i != shortestInfo.second) {
+                string insert = shortestInfo.first + "==" + p.first[i];
+                ans.push_back(insert);
+            }
+        }
+        if(p.second.size() != 0) {
+            for(int i = 0 ; i < p.second.size(); ++i) {
+                string insert = shortestInfo.first + "!=" + p.second[i];
+                ans.push_back(insert);
+            }   
+        }
+    }
+        
+        int i = 0;
+        for(; i < ans.size() - 1 ; ++i) {
+            cout << ans[i] << "&&";
+        }
+        cout << ans[i] << endl;
+
+    /*
     for(const auto& p : index) {
         for(const auto& q : p.first)  {
             cout << q << ' ';
@@ -76,6 +211,7 @@ void solve(vector<pair< pair<string, string>, int> >& set) {
         }
         cout << endl << endl;
     }
+    */
 
 }
 
